c42ed6d72cbace15bb37a59c0a2c176066746e22
Mateusz Mat <01178563@pw.edu.pl>
2023-10-18 21:50:32 +0200

    grad_desc

--- solver.py
+++ solver.py
@@ -10,15 +10,31 @@ def solver (func, x0, params=learning_rates, max_iter=1000, stopper=0.0001):
     gradient = grad(func)
     for learn_rate in params:
         x = x0
-    # x= x0
-        for _ in range(max_iter):
-        
-            x = x - (learn_rate * gradient(x)) # aktualizacja kroku
 
-            gradient_magnitude = np.abs(gradient(x))
-            if(gradient_magnitude < stopper):
-                print(f"param: {learn_rate} - very close to 0->   \t")
+        # for _ in range(max_iter):
+        #     x = x - (learn_rate * gradient(x)) # aktualizacja kroku
+
+        #     gradient_magnitude = np.abs(gradient(x))
+
+        #     if(gradient_magnitude < stopper):
+        #         print(f"param: {learn_rate} - very close to 0->   \t")
+        #         break
+
+        for _ in range(max_iter):
+            previous_grad = gradient(x)
+            previous_func_val = func(x)
+
+            x = x - (learn_rate * gradient(x)) # aktualizacja //zmniejszenie wartosci funkcji celu i zblizanie sie do minimum
+
+            if(x==0.0):
+                print(f"X TO 0 -> iteracja: {_}")
                 break
+            
+            if(abs(previous_grad - gradient(x)) < stopper and abs(previous_func_val - func(x)) < stopper):
+                print(f"param: {learn_rate} - very close to 0-> {_}  \t")
+                break
+
+
         best.append((x,f"learn: {learn_rate}" ))
 
     best.sort()
