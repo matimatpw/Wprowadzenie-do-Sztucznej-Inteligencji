50b9b370e17f15f440c1f39f24ef068cce49fca4
Mateusz Mat <01178563@pw.edu.pl>
2023-10-18 16:29:14 +0200

    dziala gradient descent

--- solver.py
+++ solver.py
@@ -5,24 +5,24 @@ import numpy as np
 def target_func(x):
     return 4*x**2 # 4x^2
 learning_rates = np.array([0.1,0.01,0.001])
-def solver (func, x0, learn_rates=learning_rates, max_iter=1000, stopper=0.0001):
+def solver (func, x0, params=learning_rates, max_iter=1000, stopper=0.0001):
     best = []
     gradient = grad(func)
-    # for learn_rate in learn_rates:
-    #     x = x0
-    x= x0
-    for _ in range(max_iter):
-    
-        x = x - (0.01 * gradient(x)) # aktualizacja kroku
+    for learn_rate in params:
+        x = x0
+    # x= x0
+        for _ in range(max_iter):
+        
+            x = x - (learn_rate * gradient(x)) # aktualizacja kroku
 
-        gradient_magnitude = np.abs(gradient(x))
-        if(gradient_magnitude < stopper):
-            print(f"very close->   \t")
-            break
-        # best.append((x,f"learn: {learn_rate}" ))
+            gradient_magnitude = np.abs(gradient(x))
+            if(gradient_magnitude < stopper):
+                print(f"param: {learn_rate} - very close to 0->   \t")
+                break
+        best.append((x,f"learn: {learn_rate}" ))
 
-
-    print(x)
+    best.sort()
+    print(best)
 
 
 solver(target_func,3.)
