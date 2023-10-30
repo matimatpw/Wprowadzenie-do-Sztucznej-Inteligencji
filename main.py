afe1cfa1b6e8f3aaeaa80c2a65c366ac461bda35
Mateusz Mat <01178563@pw.edu.pl>
2023-10-30 21:20:51 +0100

    blacked

--- main.py
+++ main.py
@@ -6,34 +6,43 @@ import time
 import json
 import warnings
 
+
 class objective_functions_factory:
     def __init__(self) -> None:
         self._objective_function_alpha_1 = self.create_objective_function(1)
         self._objective_function_alpha_10 = self.create_objective_function(10)
         self._objective_function_alpha_100 = self.create_objective_function(100)
 
-    def create_objective_function(self,my_alhpa:int) -> partial:
+    def create_objective_function(self, my_alhpa: int) -> partial:
         return partial(objective_function, alpha=my_alhpa)
-        
+
     @property
     def alpha_1(self) -> partial:
         return self._objective_function_alpha_1
-    
+
     @property
     def alpha_10(self) -> partial:
         return self._objective_function_alpha_10
-    
+
     @property
     def alpha_100(self) -> partial:
         return self._objective_function_alpha_100
-    
+
 
 # objective_function_alpha_1 = partial(objective_function, alpha=1)
 # objective_function_alpha_10 = partial(objective_function, alpha=10)
 # objective_function_alpha_100 = partial(objective_function, alpha=100)
 
+
 class result:
-    def __init__(self, beta_01:optim_params,beta_001:optim_params,beta_0001:optim_params, x:np.array, obj_fun:objective_functions_factory) -> None:
+    def __init__(
+        self,
+        beta_01: optim_params,
+        beta_001: optim_params,
+        beta_0001: optim_params,
+        x: np.array,
+        obj_fun: objective_functions_factory,
+    ) -> None:
         self.beta_01 = beta_01
         self.beta_001 = beta_001
         self.beta_0001 = beta_0001
@@ -44,38 +53,57 @@ class result:
         self.results_alfa10 = []
         self.results_alfa100 = []
 
-    def create_results_beta01(self,obj_func_alpha: partial, result_list: [optim_result], my_time:{},alpha_str:str) -> None:
+    def create_results_beta01(
+        self,
+        obj_func_alpha: partial,
+        result_list: [optim_result],
+        my_time: {},
+        alpha_str: str,
+    ) -> None:
         time_start = time.time()
         my_result = solver(obj_func_alpha, self.x, self.beta_01)
-        # print(my_result) #########################
 
         time_stop = time.time()
         result_list.append(my_result)
-        my_time[alpha_str]["Beta=0.1"] = (time_stop - time_start)
+        my_time[alpha_str]["Beta=0.1"] = time_stop - time_start
 
-    def create_results_beta001(self,obj_func_alpha:partial, result_list:[optim_result], my_time:{},alpha_str:str) -> None:
+    def create_results_beta001(
+        self,
+        obj_func_alpha: partial,
+        result_list: [optim_result],
+        my_time: {},
+        alpha_str: str,
+    ) -> None:
         time_start = time.time()
         my_result = solver(obj_func_alpha, self.x, self.beta_001)
-        # print(my_result) #########################
 
         time_stop = time.time()
         result_list.append(my_result)
-        my_time[alpha_str]["Beta=0.01"] = (time_stop - time_start)
-    
-    def create_results_beta0001(self,obj_func_alpha:partial,result_list: [optim_result],my_time:{},alpha_str:str) -> None:
+        my_time[alpha_str]["Beta=0.01"] = time_stop - time_start
+
+    def create_results_beta0001(
+        self,
+        obj_func_alpha: partial,
+        result_list: [optim_result],
+        my_time: {},
+        alpha_str: str,
+    ) -> None:
         time_start = time.time()
         my_result = solver(obj_func_alpha, self.x, self.beta_0001)
-        # print(my_result) #########################
 
         time_stop = time.time()
         result_list.append(my_result)
-        my_time[alpha_str]["Beta=0.001"] = (time_stop - time_start)
+        my_time[alpha_str]["Beta=0.001"] = time_stop - time_start
 
 
-def graph(my_result_list:[optim_result], filename:str, alpha:int)-> None:
+def graph(my_result_list: [optim_result], filename: str, alpha: int) -> None:
     for result in my_result_list:
         print(result)
-        plt.scatter(result.get_iterations(), result.get_func_values(), label=f"Beta = {result.learn_info}")
+        plt.scatter(
+            result.get_iterations(),
+            result.get_func_values(),
+            label=f"Beta = {result.learn_info}",
+        )
 
     plt.xlabel("Iterations")
     plt.ylabel("Function values")
@@ -84,10 +112,16 @@ def graph(my_result_list:[optim_result], filename:str, alpha:int)-> None:
     plt.savefig(filename, format="pdf")
 
 
-def graph_each_beta(my_result_list:[optim_result], base_filename:str, alpha:int) -> None:
+def graph_each_beta(
+    my_result_list: [optim_result], base_filename: str, alpha: int
+) -> None:
     for i, result in enumerate(my_result_list):
         print(result)
-        plt.scatter(result.get_iterations(), result.get_func_values(), label=f"Beta = {result.learn_info}")
+        plt.scatter(
+            result.get_iterations(),
+            result.get_func_values(),
+            label=f"Beta = {result.learn_info}",
+        )
 
         plt.xlabel("Iterations")
         plt.ylabel("Function values")
@@ -105,47 +139,69 @@ graph_alfa100_each_beta = partial(graph_each_beta, alpha=100)
 
 
 def main() -> None:
-    x = np.random.uniform(-100.,100.,size=10)
+    x = np.random.uniform(-100.0, 100.0, size=10)
 
     obj_function = objective_functions_factory()
-    
-    optim_param_beta_01 = optim_params(0.1,1000,0.0001)
-    optim_param_beta_001 = optim_params(0.01,1000,0.0001)
-    optim_param_beta_0001 = optim_params(0.001,1000,0.0001)
 
-    my_result = result(optim_param_beta_01,optim_param_beta_001,optim_param_beta_0001,x,obj_function)
-    my_time = {"alpha1":{"Beta=0.1":None},
-               "alpha10":{"Beta=0.01":None},
-               "alpha100":{"Beta=0.001":None}}
+    optim_param_beta_01 = optim_params(0.1, 1000, 0.0001)
+    optim_param_beta_001 = optim_params(0.01, 1000, 0.0001)
+    optim_param_beta_0001 = optim_params(0.001, 1000, 0.0001)
+
+    my_result = result(
+        optim_param_beta_01,
+        optim_param_beta_001,
+        optim_param_beta_0001,
+        x,
+        obj_function,
+    )
+    my_time = {
+        "alpha1": {"Beta=0.1": None},
+        "alpha10": {"Beta=0.01": None},
+        "alpha100": {"Beta=0.001": None},
+    }
 
     #    ## results for alpha = 1
-    my_result.create_results_beta01(obj_function.alpha_1,my_result.results_alfa1, my_time, "alpha1")
-    my_result.create_results_beta001(obj_function.alpha_1,my_result.results_alfa1,my_time,"alpha1")
-    my_result.create_results_beta0001(obj_function.alpha_1,my_result.results_alfa1, my_time,"alpha1")
-    # graph_alfa_1(my_result.results_alfa1, "alfa_1_betas.pdf")
-    
-
+    my_result.create_results_beta01(
+        obj_function.alpha_1, my_result.results_alfa1, my_time, "alpha1"
+    )
+    my_result.create_results_beta001(
+        obj_function.alpha_1, my_result.results_alfa1, my_time, "alpha1"
+    )
+    my_result.create_results_beta0001(
+        obj_function.alpha_1, my_result.results_alfa1, my_time, "alpha1"
+    )
+    graph_alfa_1(my_result.results_alfa1, "alfa_1_betas.pdf")
 
     #   ## results for alpha = 10
-    my_result.create_results_beta01(obj_function.alpha_10,my_result.results_alfa10, my_time, "alpha10")
-    my_result.create_results_beta001(obj_function.alpha_10,my_result.results_alfa10, my_time, "alpha10")
-    my_result.create_results_beta0001(obj_function.alpha_10,my_result.results_alfa10, my_time, "alpha10")
+    # my_result.create_results_beta01(
+    #     obj_function.alpha_10, my_result.results_alfa10, my_time, "alpha10"
+    # )
+    # my_result.create_results_beta001(
+    #     obj_function.alpha_10, my_result.results_alfa10, my_time, "alpha10"
+    # )
+    # my_result.create_results_beta0001(
+    #     obj_function.alpha_10, my_result.results_alfa10, my_time, "alpha10"
+    # )
     # graph_alfa_10(my_result.results_alfa10, "alfa_10_betas.pdf")
 
-
-      # results for alpha = 100
-    my_result.create_results_beta01(obj_function.alpha_100,my_result.results_alfa100, my_time, "alpha100")
-    my_result.create_results_beta001(obj_function.alpha_100,my_result.results_alfa100, my_time, "alpha100")
-    my_result.create_results_beta0001(obj_function.alpha_100,my_result.results_alfa100, my_time, "alpha100")
+    # results for alpha = 100
+    # my_result.create_results_beta01(
+    #     obj_function.alpha_100, my_result.results_alfa100, my_time, "alpha100"
+    # )
+    # my_result.create_results_beta001(
+    #     obj_function.alpha_100, my_result.results_alfa100, my_time, "alpha100"
+    # )
+    # my_result.create_results_beta0001(
+    #     obj_function.alpha_100, my_result.results_alfa100, my_time, "alpha100"
+    # )
     # graph_alfa_100(my_result.results_alfa100, "alfa_100_betas.pdf") # --> weird plot
     # graph_alfa100_each_beta(my_result.results_alfa100, "alfa_100_betas.pdf")
 
     print(my_time)
 
     with open("solver_times.json", "w") as file:
-        json.dump(my_time, file,indent=4)
+        json.dump(my_time, file, indent=4)
+
 
 if __name__ == "__main__":
-    
-    # warnings.filterwarnings("ignore", category=RuntimeWarning)
     main()
--- solver.py
+++ solver.py
@@ -2,96 +2,102 @@ from autograd import grad
 import numpy as np
 from functools import partial
 
+
 class optim_result:
-    def __init__(self, beta :float) -> None:
-        self.learn_rate_info = beta # PARAMETR KROKU
+    def __init__(self, beta: float) -> None:
+        self.learn_rate_info = beta  # PARAMETR KROKU
 
         self.func_value_history = []
         self.iter_history = []
 
         self.iteration_stop_number = None
         self.stop_information_toggle = None
-
         # self.final_time = None # ?
 
-
-    def add_func_value(self,fx:float) -> None:
+    def add_func_value(self, fx: float) -> None:
         self.func_value_history.append(fx)
 
-    def add_iter(self, iter:int) -> None:
+    def add_iter(self, iter: int) -> None:
         self.iter_history.append(iter)
 
-    def set_iter_stop(self, iter:int) -> None:
+    def set_iter_stop(self, iter: int) -> None:
         self.iteration_stop_number = iter
 
-    def set_stop_info_toggle(self,info:str)->None:
+    def set_stop_info_toggle(self, info: str) -> None:
         self.stop_information_toggle = info
 
     @property
     def last_func_value(self) -> float:
-        if(not self.func_value_history):
+        if not self.func_value_history:
             raise Exception("List is empty! No last value!")
         return self.func_value_history[-1]
 
     @property
-    def learn_info(self)-> float:
+    def learn_info(self) -> float:
         return self.learn_rate_info
-    
+
     @property
-    def stop_info_toggle(self)-> bool:
+    def stop_info_toggle(self) -> bool:
         return self.stop_information_toggle
 
     @property
-    def iter_stop_num(self)->int:
+    def iter_stop_num(self) -> int:
         return self.iteration_stop_number
-    
+
     def get_iterations(self) -> []:
         return self.iter_history
 
-    
     def get_func_values(self) -> []:
         return self.func_value_history
 
     def __str__(self) -> str:
         body = f"**Learn_rate-> {self.learn_info}\n*Last f(x) value-> {self.get_func_values()[-1]}\n"
-        if(self.stop_info_toggle):
+        if self.stop_info_toggle:
             return f"{body}*Function reached max_iteration_limit and exited!\n"
         return f"{body}*Function >fullfilled< stop_condition and exited on iteration > {self.iter_stop_num} <\n"
 
 
 class optim_params:
-    def __init__(self, beta:float, max_iterations:int, toll: float) -> None:
-        self.my_learn_rate = beta # learning_rate
+    def __init__(self, beta: float, max_iterations: int, toll: float) -> None:
+        self.my_learn_rate = beta  # learning_rate
         self.my_max_iter = max_iterations
         self.my_toll = toll
 
     @property
     def learn_rate(self) -> float:
         return self.my_learn_rate
-    
+
     @property
     def max_iter(self) -> int:
         return self.my_max_iter
 
     @property
-    def toll(self) ->float:
+    def toll(self) -> float:
         return self.my_toll
 
+
 #########################################################################
-def objective_function(x :np.array, alpha:int) -> float:
-    size = np.size(x) # -> 10
-    if(size <=1):
+def objective_function(x: np.array, alpha: int) -> float:
+    size = np.size(x)  # -> 10
+    if size <= 1:
         raise Exception("x must be atleast 2-dimension vector")
-    sum_values = np.arange(1, size + 1) # [1,2 ... 10]
+    sum_values = np.arange(1, size + 1)  # [1,2 ... 10]
     alphas_vector = alpha ** ((sum_values - 1) / (size - 1))
     final_vector = np.square(x) * alphas_vector
     result = np.sum(final_vector)
     return result
+
+
 #########################################################################
 
 
-def solver (func: callable, x0: np.array, params: optim_params, condition_toggle: bool = True, stop_toggle: bool = True) -> optim_result:
-
+def solver(
+    func: callable,
+    x0: np.array,
+    params: optim_params,
+    condition_toggle: bool = True,
+    stop_toggle: bool = True,
+) -> optim_result:
     my_result = optim_result(params.learn_rate)
 
     gradient = grad(func)
@@ -100,18 +106,19 @@ def solver (func: callable, x0: np.array, params: optim_params, condition_toggle
     iter_info = params.max_iter
 
     for iteration in range(params.max_iter):
-
         previous_func_val = func(x0)
 
         my_result.add_iter(iteration)
         my_result.add_func_value(previous_func_val)
 
-
         new_x = x0 - (learn_rate * gradient(x0))
-        x0 = new_x # x update
+        x0 = new_x  # x update
 
-        if(condition_toggle):
-            if(abs(previous_func_val - func(new_x)) < params.toll or np.linalg.norm(gradient(new_x)) < params.toll):
+        if condition_toggle:
+            if (
+                abs(previous_func_val - func(new_x)) < params.toll
+                or np.linalg.norm(gradient(new_x)) < params.toll
+            ):
                 iter_info = iteration
                 stop_toggle = False
                 break
@@ -123,20 +130,19 @@ def solver (func: callable, x0: np.array, params: optim_params, condition_toggle
 
 
 def main():
-#     objective_function_alfa = partial(objective_function, alpha=1)
-#     # --SECTION TO CHOSE VARIABLES-- #
-#     my_max_iter = 1000         # iteration limit
-#     my_toll = 0.0001           # stopper ( Elipse value )
-#     array =      np.random.uniform(-100.,100.,size=10)
-#     print(array)
+    objective_function_alfa = partial(objective_function, alpha=1)
 
-# #                            (beta,  max_iter,   toll )
-#     parameters = optim_params(0.1, my_max_iter,my_toll)
-#     output =  solver(objective_function_alfa,array, parameters,False)
-#     print(output)
+    my_max_iter = 1000  # iteration limit
+    my_toll = 0.0001  # stopper ( Elipse value )
+    array = np.random.uniform(-100.0, 100.0, size=10)
+    print(array)
+
+    #                            (beta,  max_iter,   toll )
+    parameters = optim_params(0.1, my_max_iter, my_toll)
+    output = solver(objective_function_alfa, array, parameters, False)
+    print(output)
     pass
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
-    array =      np.random.uniform(-100.,100.,size=10)
-    print(array)
