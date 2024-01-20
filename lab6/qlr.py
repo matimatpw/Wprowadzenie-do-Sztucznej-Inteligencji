92aa414ba650c06d1076bf51ff8ce5f4eb769520
Mateusz Mat <01178563@pw.edu.pl>
2024-01-20 11:26:21 +0100

    fix

--- lab6/qlr.py
+++ lab6/qlr.py
@@ -3,6 +3,24 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+class data:
+    def __init__(self, importance_future, num_episodes, evaluate_episodes, steps_limit, epsilon_val, temperatures, learning_rates, exploration_strategy='epsilon-greedy') -> None:
+        self.env = gym.make('Taxi-v3')
+        self.importance_future = importance_future
+        self.num_episodes = num_episodes
+        self.evaluate_episodes = evaluate_episodes
+        self.steps_limit = steps_limit
+        self.epsilon_value = epsilon_val
+        self.temperatures = temperatures
+        self.learning_rates = learning_rates
+        self.exploration_strategy = exploration_strategy
+
+    def set_temperature(self, temperature):
+        self.temperature = temperature
+
+    def set_learning_rate(self, learning_rate):
+        self.learning_rate = learning_rate
+
 def epsilon_greedy(q_values, epsilon):
     if np.random.rand() < epsilon:
         action = np.random.choice(len(q_values))
@@ -15,40 +33,38 @@ def boltzmann_exploration(q_values, temperature):
     action = np.random.choice(len(q_values), p=probabilities)
     return action
 
-def q_learning(env, learning_rate=0.1, importance_future=0.9, exploration_prob=0.1, num_episodes=5000, temperature=1.0, exploration_strategy='epsilon-greedy', max_steps=50):
-    state_space_size = env.observation_space.n
-    action_space_size = env.action_space.n
+def q_learning(data:data):
+    state_space_size = data.env.observation_space.n
+    action_space_size = data.env.action_space.n
 
     q_table = np.zeros((state_space_size, action_space_size))
-    
 
-    for episode in range(num_episodes):
+    for episode in range(data.num_episodes):
 
-        state = env.reset()[0]
+        state = data.env.reset()[0]
         done = False
         steps = 0
 
         episode_reward = 0
-        while not done and steps < max_steps:
+        while not done and steps < data.steps_limit:
             steps += 1
-            if exploration_strategy == 'epsilon-greedy':
-                action = epsilon_greedy(q_table[state], exploration_prob)
-            elif exploration_strategy == 'boltzmann':
-                action = boltzmann_exploration(q_table[state], temperature)
+            if data.exploration_strategy == 'epsilon-greedy':
+                action = epsilon_greedy(q_table[state], data.epsilon_value)
+            elif data.exploration_strategy == 'boltzmann':
+                action = boltzmann_exploration(q_table[state], data.temperature)
             else:
                 raise ValueError("Invalid exploration strategy")
 
-            next_state, reward, done, _, _ = env.step(action)
+            next_state, reward, done, _, _ = data.env.step(action)
 
-            q_table[state, action] = (1 - learning_rate) * q_table[state, action] + learning_rate * (reward + importance_future * np.max(q_table[next_state, :]))
+            q_table[state, action] = (1 - data.learning_rate) * q_table[state, action] + data.learning_rate * (reward + data.importance_future * np.max(q_table[next_state, :]))
             state = next_state
 
             episode_reward += reward 
 
-
     return q_table
 
-def evaluate_policy(q_table, env, num_episodes=100, epsilon=0.1, temperature=1.0, exploration_strategy='epsilon-greedy', max_steps=50):
+def evaluate_policy(data:data, q_table):
     total_rewards = 0
     episode_rewards = []
     episode_steps = []
@@ -57,7 +73,7 @@ def evaluate_policy(q_table, env, num_episodes=100, epsilon=0.1, temperature=1.0
     counter_arr = []
     counter_arr_rewards = []
     counter_rewards = 0
-    for episode in range(num_episodes):
+    for episode in range(data.evaluate_episodes):
 
         if episode % 100 == 0 and episode != 0:
             counter += 1
@@ -65,21 +81,21 @@ def evaluate_policy(q_table, env, num_episodes=100, epsilon=0.1, temperature=1.0
             counter_arr_rewards.append(counter_rewards / 100)
             counter_rewards = 0
 
-        state = env.reset()[0]
+        state = data.env.reset()[0]
         done = False
         episode_reward = 0 
         
         steps = 0
-        while not done and steps < max_steps:
-            if exploration_strategy == 'epsilon-greedy':
-                action = epsilon_greedy(q_table[state], epsilon)
-            elif exploration_strategy == 'boltzmann':
-                action = boltzmann_exploration(q_table[state], temperature)
+        while not done and steps < data.steps_limit:
+            if data.exploration_strategy == 'epsilon-greedy':
+                action = epsilon_greedy(q_table[state], data.epsilon_value)
+            elif data.exploration_strategy == 'boltzmann':
+                action = boltzmann_exploration(q_table[state], data.temperature)
             else:
                 raise ValueError("Invalid exploration strategy")
 
             steps += 1
-            state, reward, done, _, _ = env.step(action)
+            state, reward, done, _, _ = data.env.step(action)
             episode_reward += reward 
 
         total_rewards += episode_reward
@@ -88,37 +104,41 @@ def evaluate_policy(q_table, env, num_episodes=100, epsilon=0.1, temperature=1.0
         episode_rewards.append(episode_reward)
         episode_steps.append(steps)
 
-    
-    average_reward = total_rewards / num_episodes
+    average_reward = total_rewards / data.evaluate_episodes
 
     return average_reward, episode_rewards, episode_steps, counter_arr, counter_arr_rewards
 
-env = gym.make('Taxi-v3')
+# env = gym.make('Taxi-v3')
 
-importance_future = 0.9
-num_episodes = 4000
+# importance_future = 0.9
+# num_episodes = 4000
 
-evaluate_episodes = 1000
-steps_limit = 50
+# evaluate_episodes = 1000
+# steps_limit = 50
 
 
-epsilon_value = 0.1
-temperatures = [1.0, 5.0 , 10.0]
-learning_rates = [0.1, 0.5, 0.9]
+# epsilon_value = 0.1
+# temperatures = [1.0, 5.0 , 10.0]
+# learning_rates = [0.1, 0.5, 0.9]
 
 
 
 
 def main():
-    for learning_rate in learning_rates:
-        for temperature in temperatures:
+    input_data = data(importance_future=0.9, num_episodes=4000, evaluate_episodes=1000, steps_limit=50, epsilon_val=0.1, temperatures=[1.0, 5.0 , 10.0], learning_rates= [0.1, 0.5, 0.9])
 
-            q_table_epsilon = q_learning(env, learning_rate=learning_rate, exploration_prob=epsilon_value, num_episodes=num_episodes, exploration_strategy='epsilon-greedy')
-            avg_reward_epsilon, episode_rewards_greedy, episode_steps_greedy, c_x_eps, c_y_eps = evaluate_policy(q_table_epsilon, env, epsilon=epsilon_value, num_episodes=evaluate_episodes, exploration_strategy='epsilon-greedy', max_steps=steps_limit)
-            print(f'Epsilon-Greedy - Learning Rate: {learning_rate}, Average Reward: {avg_reward_epsilon}') 
+    for learning_rate in input_data.learning_rates:
+        for temperature in input_data.temperatures:
 
-            q_table_boltzmann = q_learning(env, learning_rate=learning_rate, importance_future=importance_future, temperature=temperature, num_episodes=num_episodes, exploration_strategy='boltzmann')
-            avg_reward_boltzmann, episode_rewards_boltz, episode_steps_bolts, c_x_bolt, c_y_bolt = evaluate_policy(q_table_boltzmann, env, temperature=temperature, num_episodes=evaluate_episodes, exploration_strategy='boltzmann', max_steps=steps_limit)
+            input_data.set_temperature(temperature)
+            input_data.set_learning_rate(learning_rate)
+
+            q_table_epsilon = q_learning(input_data)
+            avg_reward_epsilon, episode_rewards_greedy, episode_steps_greedy, c_x_eps, c_y_eps = evaluate_policy(input_data, q_table_epsilon)
+            print(f'Epsilon-Greedy - Learning Rate: {learning_rate}, Average Reward: {avg_reward_epsilon}')
+
+            q_table_boltzmann = q_learning(input_data)
+            avg_reward_boltzmann, episode_rewards_boltz, episode_steps_bolts, c_x_bolt, c_y_bolt = evaluate_policy(input_data, q_table_boltzmann)
             print(f'Boltzmann - Temperature: {temperature}, Average Reward: {avg_reward_boltzmann}')
 
             plt.plot(c_x_eps, c_y_eps, label=f'Epsilon-Greedy avg', color = 'red',linestyle='-', alpha=0.8)
@@ -128,7 +148,7 @@ def main():
             plt.xlabel('Episode Number')
             plt.ylabel('Episode Reward')
             plt.legend()
-            plt.savefig(f'Olr - lr_ {learning_rate} & T_ {temperature}.pdf')
+            plt.savefig(f'XXXOlr - lr_ {learning_rate} & T_ {temperature}.pdf')
             plt.close()
 
             print("PLOT DONE")
